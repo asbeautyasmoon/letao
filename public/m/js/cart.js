dd84deccff78a852face23c361493ca8e266b570
lll <1822981928@qq.com>
2018-11-14 20:03:01 +0800

    购物车完成

--- public/m/js/cart.js
+++ public/m/js/cart.js
@@ -10,7 +10,7 @@ function getdata(canshu,fn){
 	 		
 	 	}
 	 	else{
-	 		//console.log(r);
+	 		console.log(r);
 	 		if(r.length!=0){  //需要判断下再生成，不然因为一次一次取没有数据了或报错
 		 		for (var i = 0; i < r.data.length; i++) {
 			 				var picad=r.data[i].pic[0].picAddr || 'images/none.png';
@@ -108,20 +108,132 @@ $('.gl').on('tap',function () {
 
  //绑定委托事件,编辑功能
 $('.container').on('tap','a',function () {
-	//console.log('hh');	
-			// $.get('',{},function(r) {
-			// 	 	if(r.error==400){
-			// 	 		//window.stop();
-			// 	 		location.href = '/m/user/login.html' + '?returnUrl=' + location.href;		 		
-			// 	 	}
-			// 	 	else{
-				 			 	
-			// 	 	    console.log(r);
+	//console.log('hh');
+			var gwcid=$(this).prev().data('sxid');
+			var nowclick=$(this);
+			console.log(gwcid);		
+	        mui.alert('正为您打开编辑','请稍等','好的',function () {},'div');	
+			$.get('/cart/queryCart',{},function(r) {
+				 	if(r.error==400){
+				 		//window.stop();
+				 		location.href = '/m/user/login.html' + '?returnUrl=' + location.href;		 		
+				 	}
+				 	else{
+				 		mui.closePopup();   //关闭刚打开的对话框	
+				 	    console.log(r);
+				 	    //console.log($(this));	
+				 	    var cmxz='' ;
+				 	    var shu='';
+				 	    for (var i = 0; i < r.length; i++) {
+				 	    	if (r[i].id==gwcid) {
+				 	    		 var sizer=r[i].productSize.split('-');   //渲染尺码
+				 	    	     console.log(sizer);  //注意 split后是字符串
+				 	    		 for (var j = parseInt(sizer[0]); j < parseInt(sizer[1])+1; j++) {
+				 	    		 	cmxz=cmxz+  "<span class='cm'>"+String(j)+"</span>" ;
+				 	    		 }
+
+				 	    		 //渲染数量
+				 	   			 shu='<section class="num"><span >数量：</span><span class="jia">+</span><span contenteditable="true" class="gms">'+r[i].num+'</span><span class="jian">-</span><span class="sy">剩余：</span><span class="sys">'+r[i].productNum+'</span><span >件</span></section>';
+				 	   			 
+				 	   			 var bjnr='<span>尺码：</span>'+cmxz+shu;
+				 			     mui.confirm(bjnr,'商品编辑',['保存','放弃'],function (mes) {
+
+				 			     		 			     			//给后台发送修改请求	没办法自己绑定 只好放在这里 	
+								 		if (mes.index == 0) {
+
+											// $('.mui-popup-buttons:first-child').on('tap',function (e) {   //这个mui不知道给按钮绑定什么事件了，用自己绑定click不成功，只能用绑定tap,而且用first等等选择按钮也不行	  
+								 			   console.log('hh');
+								 			   var nowsize=$('.mui-popup-text .cm.now').text();
+
+								 				$.post('/cart/updateCart',{'id':gwcid,'size':nowsize,'num':nownum},function(r) {
+												 	if(r.error==400){
+												 		//window.stop();
+												 		location.href = '/m/user/login.html' + '?returnUrl=' + location.href;		 		
+												 	}
+												 	else{
+												 		if (r.success) {
+												 			mui.toast('修改成功');
+
+												 			//修改成功后返回页面时仍需查询后直接修改
+												 			$.get('/cart/queryCart',{},function(r) {
+															 	if(r.error==400){
+															 		//window.stop();
+															 		location.href = '/m/user/login.html' + '?returnUrl=' + location.href;		 		
+															 	}
+															 	else{
+															 		 for (var i = 0; i < r.length; i++) {
+															 		 	if (r[i].id==gwcid) {
+															 		 		 nowclick.find('.num').text('x'+r[i].num+'双');
+															 		 		 nowclick.find('.size').text('鞋码：'+r[i].size);
+															 		 	}
+															 		 }
+															 	}
+															});
+												 															 		
+												 		}
+												 	}	
+												});
+								 		// })
+								 		}
+					
+
+
+
+
+
+
+
+
+
+
+				 			     },'div');
+				 	    		
+				 			
+				 			     $('.mui-popup-text .cm').each(function (idx,e) {
+				 			     	if($(e).text()==r[i].size){
+				 	      		 		$(e).addClass('now');
+				 	      		 	}
+				 			     });
+
+
+				 	    	}
+				 	    }
+				 	    //处理渲染后的点击选择，数量选择
+				 	   
+				 	     $('.mui-popup-text .cm').on('click',function () {
+				 	     	$(this).addClass('now').siblings().removeClass('now');
+				 	     	
+				 	     });
 				 		
-			// 	 	}
+				 		var nownum=$('.mui-popup-text .gms').text();  
+				 		$('.mui-popup-text .jia').on('click',function () {			//注 我们这里没有再进行提交时的数量再判断，超过数量照例应该后台应该在返回的，结果这个后台也是简单的按最大数进行处理了
+				 		
+				 			nownum++;
+				 			if (nownum>$('.mui-popup-text .sys').text()) {
+				 				mui.toast('超过最大数量');
+				 				nownum=$('.mui-popup-text .sys').text();
+				 			}
+				 			$('.mui-popup-text .gms').text(nownum);
+
+
+				 		});
+				 		$('.mui-popup-text .jian').on('click',function () {
+				 		   
+				 			nownum--;
+				 			if (nownum<1) {
+				 				mui.toast('数量不正确');
+				 				nownum=1;
+				 			}
+				 			$('.mui-popup-text .gms').text(nownum);
+
+				 		});
+
+				 		
+
+				 	}
 				 
 
-			// 	 });
+				 });
 	
 })
 
@@ -221,7 +333,13 @@ $('.container ul').on('click','input',function () {
 
 
 	//全选关联
-	
+	if ($('.container input:checked').length==$('.container ul input').length) {  //所有选中的复选框可以直接用jq的checked选择，所以这里就只有哟个长度判断是否全点
+		$('.dingdan input').prop("checked",true);
+	}
+	else{
+		$('.dingdan input').prop("checked",false);
+	}
+	  
 
 });
 
