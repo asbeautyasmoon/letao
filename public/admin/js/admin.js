7d4ac6a2b9eae738dc233e9a7a99f389d9d9164d
lll <1822981928@qq.com>
2018-11-17 22:17:50 +0800

    后台登陆 登出 主页

--- public/admin/js/admin.js
+++ public/admin/js/admin.js
@@ -1,69 +1,157 @@
-/*1.进度显示功能*/
-/*不显示转圈效果*/
-NProgress.configure({
-    showSpinner: false
-});
-/*在ajax开始请求的时候  把进度条显示出来*/
-$(window).ajaxStart(function(){
-    NProgress.start();
-});
-/*在ajax结束请求的时候  把进度条完成*/
-$(window).ajaxStart(function(){
-    NProgress.done();
-});
-/*2.左菜单的显示和隐藏*/
-$('[data-menu]').on('click',function(){
-    $('.ad_aside').toggle();
-    $('.ad_section').toggleClass('menu');
-});
-/*3.二级菜单的显示和隐藏*/
-$('.menu [href="javascript:;"]').on('click',function(){
-    var $this = $(this);
-    var $child = $this.siblings('.child');
-    $child.slideToggle();
-});
-/*4.退出功能*/
-$('[data-logout]').on('click',function(){
-    /*1.准备模态框*/
-    var logoutModal =
-                    '<div class="modal fade" id="logoutModal">'+
-                        '<div class="modal-dialog modal-sm">'+
-                            '<div class="modal-content">'+
-                                '<div class="modal-header">'+
-                                    '<button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>'+
-                                    '<h4 class="modal-title">温馨提示</h4>'+
-                                '</div>'+
-                                '<div class="modal-body">'+
-                                    '<p class="text-danger"><span class="glyphicon glyphicon-info-sign"></span> 您确定要退出后台管理系统吗？</p>'+
-                                '</div>'+
-                                '<div class="modal-footer">'+
-                                    '<button type="button" class="btn btn-default" data-dismiss="modal">取消</button>'+
-                                    '<button type="button" class="btn btn-primary">确定</button>'+
-                                '</div>'+
-                            '</div>'+
-                        '</div>'+
-                    '</div>';
-    /*2.把模态框追加到页面当中*/
-    $('body').append(logoutModal);
-    /*3.显示模态框*/
-    $('#logoutModal').modal('show');
-    /*4.当点击确定按钮的时候进行对出*/
-    /*5.防止多次绑定  在绑定之前先注销上一次的绑定*/
-    $('#logoutModal').off('click','.btn-primary').on('click','.btn-primary',function(){
-        /*6.发送退出请求*/
-        $.ajax({
-            type:'get',
-            url:'/employee/employeeLogout',
-            data:{},
-            dataType:'json',
-            success:function(data){
-                setTimeout(function(){
-                    if(data.success){
-                        /*7.退出成功*/
-                        location.href = 'login.html';
-                    }
-                },500);
-            }
-        });
-    });
-});
+
+	$.get('/employee/checkRootLogin',{},function(r){    //接口判断是否登录
+		console.log(r);
+		if (r.success) {  
+			//这个后端也无语了，判断了登陆好歹也给个员工头像地址等等的，什么也没有
+			$('.out').on('click',function(){
+				$('#myModal').fadeIn();   //看bts的代码，这个模态框原来的js也是有淡入的效果的，所以我这里用fadein，而默认的fade类就被我去除了
+
+			});
+			$('.modal-footer .sure').on('click',function(){    //确定后 有返回成功时退出
+				$.get('/employee/employeeLogout',{},function(r){
+					if (r.success) {
+						location.href='login.html';
+					}
+					
+				});
+			});
+			$('[data-dismiss]').on('click',function(){
+				$('#myModal').fadeOut();
+			})
+
+	//使用echarts绘制图表
+			    /*假设的数据*/
+			    var data = [  
+			        {
+			            name:'一月',
+			            value:300
+			        },
+			        {
+			            name:'二月',
+			            value:400
+			        },
+			        {
+			            name:'三月',
+			            value:500
+			        },
+			        {
+			            name:'四月',
+			            value:200
+			        },
+			        {
+			            name:'五月',
+			            value:600
+			        }
+			    ];
+			    var xdata = [], sdata = [];     //横坐标的名称数组格式 纵坐标的数字值也是数组格式
+			 	for (var i = 0; i < data.length; i++) {
+			 			xdata.push(data[i].name);
+			 			sdata.push(data[i].value);
+			 		}	
+			 	//console.log(sdata);
+			 	
+			 var myChart = echarts.init($('#zzt')[0]);    //初始化，注意要js的dom
+			 var option = {
+			 				title:{
+					            text:'2017年注册人数'      //表的标题
+					        },	
+					        legend:{					  //图例组件展现了不同系列的标记(symbol)，颜色和名字		
+					            data:[{name:'注册人数',}]   //  该项的名字，注意要对应下面的series的name  
+					        },
+					          tooltip : {                 //点击柱时的说明
+      						 },
+
+				   		   xAxis: {
+						        type: 'category',
+						        //data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+						    },
+						    yAxis: {
+						        type: 'value'
+						    },
+						    series: [{
+						        //data: [120, 200, 150, 80, 70, 110, 130],
+						        type: 'bar',				//柱状图
+						        name:'注册人数',            // 点击时的说明
+						        barWidth: '60%',
+
+						    }]
+						};
+				  option.xAxis.data=xdata;  //把数组传给配置参数
+				  option.series[0].data=sdata;    //注意series是个数组，不是纯对象
+
+		     myChart.setOption(option);
+		
+		    //饼状图 假设数据未格式化导入
+		     var myChart2 = echarts.init($('#bzt')[0]);
+		     	option = {
+				    title : {
+				         text: '品牌销售占比',     
+				         subtext: '2017年10月',
+				         x:'center'                //标题位置
+				    },
+				    tooltip : {
+				     trigger: 'item',          //数据项图形触发提示，主要在散点图，饼图等无类目轴的图表中使用。
+							///*series.name  a  */				 //数据格式
+				            /*data.name  b */
+				            /*data.value  c */
+				            /*占比  d */
+				        formatter: "  {b} : {c} ({d}%)"    
+				    },
+				    legend: {
+				        orient: 'vertical',
+				        left: '20%',     //图例块左右位置
+				        data: ['李宁','耐克','阿迪','匡威','回力'],
+				    },
+				    series : [
+				        {	
+				        	color:['#c23','#2f5', '#61a', '#d48', '#91c','#749f83',  '#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'],   //可设置各项依次的颜色 没有设置的话会用默认的
+				            name: '销量',
+				            type: 'pie',
+				            radius : '55%',				//饼图的半径。可以为如下类型：number：直接指定外半径值。'20%'，表示外半径为可视区尺寸（容器高宽中较小一项）的 20% 长度。
+				            center: ['50%', '60%'],       //饼图的中心（圆心）坐标，数组的第一项是横坐标，第二项是纵坐标。
+				            data:[
+				                {value:335, name:'李宁'},
+			                    {value:310, name:'耐克'},
+			                    {value:234, name:'阿迪'},
+			                    {value:135, name:'匡威'},
+			                    {value:1548, name:'回力'}
+				            ],
+				            itemStyle: {
+				            	normal:{       	       //非点击时的样式 统一的样式
+				            		// color: ['#a12'],       //设置了会覆盖了上面设置的颜色
+				            	},	
+				                emphasis: {				//高亮的扇区和标签样式。    这里跟文档的配置相反了 文档中 shadowBlur在 itemStyle里
+				                    shadowBlur: 100,    //阴影大小
+				                    shadowOffsetX: 0,	
+				                    shadowColor: 'rgba(0, 0, 0, 0.5)',
+				                    borderColor:'red',   //高亮时的框线
+				                }
+				            }
+				        }
+				    ]
+				};
+			myChart2.setOption(option);	
+
+
+
+
+		}
+		else{
+			//不需要再写跳转，这里发送请求如果是未登录直接自己做了跳转；
+			return;
+		}
+
+	});
+ 
+
+//进度条
+// NProgress.configure({ showSpinner: false });
+// $(window).ajaxStart(function () {
+//     /*只要使用的ajax就会执行这个方法*/
+//     //开启进度条
+//     NProgress.start();
+// });
+// $(window).ajaxComplete(function () {
+//     /*结束进度条*/
+//     NProgress.done();
+// });
--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,84 +1,117 @@
-$(function(){
-    /*前端校验功能  bootstrap validator*/
-    /*1.完整的表单结构  form   input  submit 这些元素*/
-    /*2.表单元素需要对应的名字 name="username" */
-    /*3.初始化表单验证组件 插件*/
-    /*4.配置组件功能*/
-    /*5.配置具体的属性需要的校验规则*/
-    $('#login').bootstrapValidator({
-        /*提示的图标*/
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        /*属性对应的是表单元素的名字*/
-        fields:{
-            /*配置校验规则*/
-            username:{
-                /*规则*/
-                validators: {
-                    notEmpty: {
-                        message: '用户名不能为空'
-                    },
-                    /*设置错误信息 和规则无关 和后台校验有关系*/
-                    callback: {
-                        message: '用户名错误'
+//表单验证插件：
+//配置规则：
+//			notEmpty：非空验证；
+// 			stringLength：字符串长度验证；
+// 			regexp：正则表达式验证；
+// 			emailAddress：邮箱地址验证（都不用我们去写邮箱的正则了~~
+// 			base64：64位编码验证；
+// 			between：验证输入值必须在某一个范围值以内，比如大于10小于100；
+// 			creditCard：身份证验证；
+// 			date：日期验证；
+// 			ip：IP地址验证；
+// 			numeric：数值验证；
+// 			phone：电话号码验证；
+// 			uri：url验证；
+// 			callback:自定义的错误名称，好像用随意的名称不行
+//
+//常用事件：
+//重置某一单一验证字段验证规则 $(formName).data(“bootstrapValidator”).updateStatus("fieldName",  状态,  规则 ); ---/*NOT_VALIDATED 还没校验, VALIDATING 校验中, INVALID 失败 or VALID 成功*/
+//手动触发表单验证 $(formName).data(“bootstrapValidator”).validateField('fieldName');
+//如果不用按钮禁用，单纯想在验证成功后才触发事件：var bootstrapValidator = form.data('bootstrapValidator');
+												// bootstrapValidator.on('success.form.bv', function (e) {  或者直接在 $('form').bootstrapValidator(）.on('success.form.bv', function (e))
+    								// 				e.preventDefault();
+   									// 							 //提交逻辑
+												// 				});
+//
+	 $(function () {
+        $('form').bootstrapValidator({
+        	live: 'enabled',//验证时机，enabled是内容有变化就验证（默认），disabled和submitted是提交再验证
+            excluded: [':disabled', ':hidden', ':not(:visible)'],//排除无需验证的控件，比如被禁用的或者被隐藏的
+            submitButtons: '.dl',//指定提交按钮 这个就是说如果验证不通过会把这个按钮变灰
+            message: '通用的验证失败消息',//?未触发
+			feedbackIcons: {
+                　　　　　　　　valid: 'glyphicon glyphicon-ok',
+                　　　　　　　　invalid: 'glyphicon glyphicon-remove',
+                　　　　　　　　validating: 'glyphicon glyphicon-refresh'
+            　　　　　　　　   },
+            fields: {
+                name: {                    //input的name
+                  
+                    validators: {
+                        notEmpty: {
+                            message: '用户名不能为空'
+                        },
+                        regexp: {//正则验证
+                                regexp: /^[a-zA-Z0-9_\.]+$/,
+                                message: '所输入的字符不符要求'
+                            },
+				        stringLength: {//检测长度
+				                min: 1,
+				                max: 10,
+				                message: '长度必须在1-10之间'
+				            },    	
+				        callback:{
+				        	 message: '用户名错误',
+				        }   	
                     }
-                }
-            },
-            password:{
-                validators:{
-                    notEmpty: {
-                        message: '密码不能为空'
-                    },
-                    stringLength:{
-                        min:6,
-                        max:18,
-                        message:'密码在6-18个字符内'
-                    },
-                    callback: {
-                        message: '密码不正确'
-                    }
-                }
-            }
-        }
-        /*7.表单校验成功*/
-    }).on('success.form.bv', function(e) {
-        /*禁用默认提交的事件 因为要使用ajax提交而不是默认的提交方式*/
-        e.preventDefault();
-        /*获取当前的表单*/
-        var $form = $(e.target);
-        /*发送登录请求*/
-        $.ajax({
-            type:'post',
-            url:'/employee/employeeLogin',
-            data:$form.serialize(),
-            dataType:'json',
-            success:function(data){
-                if(data.success){
-                    /*后台管理员 root 123456*/
-                    /*登录成功*/
-                    location.href = 'index.html';
-                }else{
-                    /*登录不成功*/
-                    /*8.恢复可提交的按钮*/
-                    $form.data('bootstrapValidator').disableSubmitButtons(false);
-                    /*9.指定某一个表单元素的错误提示*/
-                    /* NOT_VALIDATED, VALIDATING, INVALID or VALID */
-                    if(data.error == 1000){
-                        $form.data('bootstrapValidator').updateStatus('username','INVALID','callback');
-                    }else if(data.error == 1001){
-                        $form.data('bootstrapValidator').updateStatus('password','INVALID','callback');
+                },
+                pasw: {
+                    validators: {
+                        notEmpty: {
+                            message: '密码不能为空'
+                        },
+  						regexp: {//正则验证
+                                regexp: /^[a-zA-Z0-9_\.]+$/,
+                                message: '所输入的字符不符要求'
+                            },
+				        stringLength: {//检测长度
+				                min: 1,
+				                max: 10,
+				                message: '长度必须在1-10之间'
+				            },    
+				         callback:{
+				        	 message: '密码错误',
+				        },     
+
                     }
                 }
             }
         });
     });
-    /*重置功能*/
-    $('[type="reset"]').on('click',function(){
-        /*6.重置验证*/
-        $('#login').data('bootstrapValidator').resetForm();
-    });
 
-});
+
+
+
+
+
+
+//点击登陆
+$('.dl').on('click',function(){
+	 var input=$('form').serialize();
+	//把url转为json	                   
+	 var arrk=input.split('&');  
+	 var localparm={};
+	 for (var i = 0; i < arrk.length; i++) {
+	 	 var arrkd=arrk[i].split('=');     
+	 	 localparm[arrkd[0]]=decodeURI(arrkd[1]) ;    
+	 }
+
+	//注意，jq的ajax 可以直接传入.serialize()的内容，通过查看，可以看到他已经自动转为inputname：value   inputname：value的格式了
+
+	$.post('/employee/employeeLogin',{username:localparm.name,password:localparm.pasw},function(r){
+		console.log(r);
+		if (r.error==1000) {  //服务器返回错误1000是用户名错误 
+			$('form').data('bootstrapValidator').updateStatus("name",  "INVALID",'callback');
+
+		}
+		else if (r.error==1001) { 
+			$('form').data('bootstrapValidator').updateStatus("pasw",  "INVALID",'callback');
+		}
+		else if (r.success){
+			//location.href='index.html?username='+localparm.name+'&password='+localparm.pasw; //跳转时不能明文
+			 location.href='index.html';
+		}
+
+
+	});
+})
